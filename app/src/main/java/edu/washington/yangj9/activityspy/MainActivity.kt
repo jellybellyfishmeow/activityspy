5b5886259901ad4726ebe5e7322ccf3c3bed81d4
Jingyu Yang <yangj9@uw.edu>
2018-04-13 17:00:35 -0700

    finished all stories

---
+++ app/src/main/java/edu/washington/yangj9/activityspy/MainActivity.kt
@@ -0,0 +1,52 @@
+package edu.washington.yangj9.activityspy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val tag = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if(savedInstanceState == null) {
+            Log.i(tag, "onCreate event fired")
+        } else {
+            Log.i(tag, "onCreate event fired" + savedInstanceState.toString())
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(tag, "onStart fired")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(tag, "onResume fired")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(tag, "onPause fired")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(tag, "onStop fired")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(tag, "onRestart fired")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(tag, "onDestroy fired")
+        Log.e(tag, "We’re going down, Captain!")
+    }
+}
